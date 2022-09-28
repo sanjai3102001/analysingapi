93a533ab5c6dc3215f39e4ff6d0d1bb87ba8ee3c
sanjai3102001 <98545323+sanjai3102001@users.noreply.github.com>
2022-09-28 15:05:44 +0530

    This is a repo for analysing the movie api

---
+++ main_test.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	//"go-postgres/function"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/gorilla/mux"
+	"github.com/stretchr/testify/assert"
+)
+
+func Router() *mux.Router {
+	router := mux.NewRouter()
+	router.HandleFunc("/", CreateItem).Methods("GET")
+	router.HandleFunc("/movie/1", ReadingItemid).Methods("GET")
+	// router.HandleFunc("/movie", function.CreateItem).Methods("POST")
+	// router.HandleFunc("/movie/2", function.UpdateItems).Methods("PUT")
+	// router.HandleFunc("/movie/2", function.Softdelete).Methods("DELETE")
+	return router
+}
+
+func TestCreateItem(t *testing.T) {
+	request, _ := http.NewRequest("GET", "/", nil)
+	response := httptest.NewRecorder()
+	Router().ServeHTTP(response, request)
+	assert.Equal(t, 200, response.Code, "OK response is expected")
+}
+
+func TestReadItem(t *testing.T) {
+	request, _ := http.NewRequest("GET", "/movie/1", nil)
+	response := httptest.NewRecorder()
+	Router().ServeHTTP(response, request)
+	assert.Equal(t, 200, response.Code, "OK response is expected")
+}
